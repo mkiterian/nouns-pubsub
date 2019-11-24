42216aac2f6832dd03b3ff9e42fb3a06f5199b66
mkiterian <hmukite@gmail.com>
2019-11-25 00:53:31 +0300

    add preprocessors

---
+++ get_nouns.py
@@ -0,0 +1,44 @@
+import collections
+import os
+import pprint
+import requests
+from requests_oauthlib import OAuth1
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+KEY = os.getenv('THE_NOUN_PROJECT_KEY')
+SECRET = os.getenv('THE_NOUN_PROJECT_SECRET')
+auth = OAuth1(KEY, SECRET)
+items = ['fish', 'dog', 'cat', 'bird']
+
+
+def get_licence_types(icons):
+    licences = [icon['license_description'] for icon in icons]
+    return set(licences)
+
+
+def get_icon_groups(icons):
+    grouped = collections.defaultdict(list)
+    for icon in icons:
+        grouped[icon['license_description']].append(icon)
+    return dict(grouped)
+
+
+def get_icons(item):
+    icons = []
+    endpoint = 'http://api.thenounproject.com/icons/{}?limit=50'.format(item)
+
+    # not in a try block coz I need to see the traceback
+    response = requests.get(endpoint, auth=auth)
+    icons = response.json()['icons']
+    return icons
+
+
+def get_all_icons(items):
+    all_icons = []
+    for item in items:
+        all_icons.append(get_icons(item))
+    return all_icons
+
